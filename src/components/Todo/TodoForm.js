4cdf1287a17da2bd61825e4d351ee6d0201e21a9
Raginee Upadhyaya <upadhyayaraginee@gmail.com>
2022-02-23 10:37:21 -0800

    error fixed

--- src/components/Todo/TodoForm.js
+++ src/components/Todo/TodoForm.js
@@ -2,28 +2,27 @@ import React, { useState } from 'react';
 import Button from '../UI/Button/Button.js';
 import './TodoForm.css';
 
-const TodoForm = function (props) {
+const TodoForm = (props) => {
     const [enteredValue, setEnteredValue] = useState('');
-
-    const goalInputChangeHandler = (event) => {
-        setEnteredValue(event.target.value);
-    }
-    const formSubmitHandler = (event) => {
-        event.preventDefault();
-        props.onAddGoal(enteredValue)
-    }
+  
+    const goalInputChangeHandler = event => {
+      setEnteredValue(event.target.value);
+    };
+  
+    const formSubmitHandler = event => {
+      event.preventDefault();
+      props.onAddGoal(enteredValue);
+    };
+  
     return (
-        <form onSubmitHandler={formSubmitHandler}>
-            <div className='form-control'>
-                <label>Coures Goal</label>
-                <input
-                    type='text'
-                    placeholder='Add to do'
-                    onChange={goalInputChangeHandler} />
-            </div>
-            <Button type='submit'>Add Todo</Button>
-        </form>
-    )
-}
-
-export default TodoForm
+      <form onSubmit={formSubmitHandler}>
+        <div className="form-control">
+          <label>Course Goal</label>
+          <input type="text" onChange={goalInputChangeHandler} />
+        </div>
+        <Button type="submit">Add Goal</Button>
+      </form>
+    );
+  };
+  
+  export default TodoForm;
